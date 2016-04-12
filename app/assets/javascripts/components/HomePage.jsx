0724925bf47429b14136c8de974439478967ae5b
Jason Waldrip <jason@waldrip.net>
2016-04-12 16:54:46 -0600

    update home page

--- app/assets/javascripts/components/HomePage.jsx
+++ app/assets/javascripts/components/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default class HomePage extends React.Component {
-    render() {
-        return(
-            <div>
-                Welcome to the React Denver.rb Demo!
-            </div>
-        )
-    }
+  render() {
+    const header = "<h1>Hello</h1>"
+    return(
+        <div>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+            Welcome to the React Denver.rb Demo!
+        </div>
+    )
+  }
 }
