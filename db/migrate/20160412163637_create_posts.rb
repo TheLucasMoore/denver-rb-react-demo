307da3f1bd5c71547b34daf4a04cf3d3534c1fe0
Jason Waldrip <jason@waldrip.net>
2016-04-12 18:14:43 -0600

    change fields

--- db/migrate/20160412163637_create_posts.rb
+++ db/migrate/20160412163637_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :author_email
-      t.string :title
-      t.text :body
+      t.string :author_email, null: false
+      t.string :title, null: false
+      t.text :body, null: false
 
       t.timestamps null: false
     end
